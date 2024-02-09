71e5619efac1c6062dc3e3e87d420c9e03eb72f4
IdentalerMaxima <koppany.naray@gmail.com>
2024-02-09 13:55:23 +0100

    add new files for the different modules planned

---
+++ vite-project/src/views/Attendance.jsx
@@ -0,0 +1,9 @@
+import PageComponent from "../components/PageComponent";
+
+export default function Dashboard() {
+    return (
+        <PageComponent title={'Jelenleti iv modul'}>
+            Jelenleti iv modul komponensek
+        </PageComponent>
+    );
+}
