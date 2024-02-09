48807c8fcc3ae299eb8254592e6baa53ad96dd04
IdentalerMaxima <koppany.naray@gmail.com>
2024-02-09 14:55:24 +0100

    update signup div css

--- vite-project/src/views/Signup.jsx
+++ vite-project/src/views/Signup.jsx
@@ -1,63 +1,101 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axiosClient from "../axios.js";
+
 export default function Signup() {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const [error, setError] = useState({ __html: '' });
+
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    setError({ __html: '' });
+
+    axiosClient.post('/signup', {
+      name: fullName,
+      email: email,
+      password: password,
+      passwordConfirmation: passwordConfirmation
+    })
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch((error) => {
+
+        if (error.response) {
+          const finalErrors = Object.values(error.response.data.errors).reduce((acc, next) => [...next, ...acc], []);
+          console.log(finalErrors);
+          setError({ __html: finalErrors.join('<br>') });
+        }
+
+      })
+
+  }
+
   return (
     <>
       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-        Regisztráció
+        Regisztrálj most!
       </h2>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        <form onSubmit={onSubmit} className="" action="" method="POST">
 
-        <div>
+          {error.__html && (<div className="mt-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded"
+            dangerouslySetInnerHTML={error}>
+          </div>)}
+          
+          <div>
             <div className="mt-2">
               <input
                 id="fullName"
                 name="fullName"
                 type="text"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-t-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Teljes név"
               />
             </div>
           </div>
 
           <div>
-            <div className="mt-2">
+            <div className="">
               <input
                 id="email"
                 name="email"
                 type="email"
-                autoComplete="email"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Email cím"
               />
             </div>
           </div>
 
           <div>
-            <div className="mt-2">
+            <div className="">
               <input
                 id="password"
                 name="password"
                 type="password"
                 autoComplete="current-password"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Jelszó"
               />
             </div>
           </div>
 
           <div>
-            <div className="mt-2">
+            <div className="">
               <input
-                id="password_confirmation"
-                name="password_confirmation"
+                id="passwordConfirmation"
+                name="passwordConfirmation"
                 type="password"
-                autoComplete="current-password"
                 required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="block w-full rounded-b-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 placeholder="Jelszó megerősítése"
               />
             </div>
@@ -66,7 +104,7 @@ export default function Signup() {
           <div>
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full justify-center rounded-md mt-10 bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Fiók létrehozása
             </button>
@@ -74,13 +112,18 @@ export default function Signup() {
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
-					Van már fiókod?{' '}
-					<a href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-						Jelentkezz be!
-					</a>
-				</p>
-        
+          Van már fiókod?{' '}
+          <Link
+            to="/login"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Jelentkezz be!
+          </Link>
+
+        </p>
+
       </div>
     </>
   )
+
 }
