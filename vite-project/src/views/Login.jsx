e161ae7c9a9f398f61304a79229013e287491d10
IdentalerMaxima <koppany.naray@gmail.com>
2024-02-09 14:58:52 +0100

    add login placeholders

--- vite-project/src/views/Login.jsx
+++ vite-project/src/views/Login.jsx
@@ -19,6 +19,7 @@ export default function Login() {
                 autoComplete="email"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Add meg az email címedet!"
               />
             </div>
           </div>
@@ -42,6 +43,7 @@ export default function Login() {
                 autoComplete="current-password"
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Add meg a jelszavadat!"
               />
             </div>
           </div>
