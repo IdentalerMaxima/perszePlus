26b4b18093602b472a9bba3a95995246eb090485
IdentalerMaxima <koppany.naray@gmail.com>
2024-03-11 14:52:48 +0100

    Create i18n

---
+++ vite-project/src/i18n.js
@@ -0,0 +1,20 @@
+import React from "react";
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .use(Backend)
+  .use(LanguageDetector)
+  .init({
+    //lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+    }
+  });
+
+  export default i18n;
