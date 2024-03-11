d9cd6635b6c060ecdea335df733d87759c6335bd
IdentalerMaxima <koppany.naray@gmail.com>
2024-03-11 18:10:07 +0100

    Update i18n

--- vite-project/src/i18n.js
+++ vite-project/src/i18n.js
@@ -1,4 +1,3 @@
-import React from "react";
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
@@ -7,11 +6,9 @@ import Backend from "i18next-http-backend";
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .use(Backend)
-  .use(LanguageDetector)
+  // .use(LanguageDetector)
   .init({
-    //lng: "en", // if you're using a language detector, do not define the lng option
     fallbackLng: "en",
-
     interpolation: {
       escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
     }
