5cf5135e58d41c137f164e513496ee6c5c5d5730
IdentalerMaxima <koppany.naray@gmail.com>
2024-03-15 12:46:09 +0100

    save token when creating a new account

--- vite-project/src/contexts/ContextProvider.jsx
+++ vite-project/src/contexts/ContextProvider.jsx
@@ -8,12 +8,19 @@ const StateContext = createContext({
 });
 
 export const ContextProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState({
-        name: 'Bakó Berik',
-        email: 'bako@erik.com',
-        imageUrl: '..\\src\\assets\\Capture.PNG'
-    })
-    const [userToken, setUserToken] = useState('')
+    const [currentUser, setCurrentUser] = useState({})
+    const [userToken, _setUserToken] = useState(localStorage.getItem('TOKEN') || '');
+
+    const setUserToken = (token) => { 
+        if(token){
+            localStorage.setItem('TOKEN', token);
+        } else {
+            localStorage.removeItem('TOKEN');
+        }
+        _setUserToken(token);
+    }
+
+
     return (
         <StateContext.Provider value={{
             currentUser,
@@ -26,4 +33,4 @@ export const ContextProvider = ({ children }) => {
     )
 };
 
-export const userStateContext = () => useContext(StateContext);
+export const useStateContext = () => useContext(StateContext);
