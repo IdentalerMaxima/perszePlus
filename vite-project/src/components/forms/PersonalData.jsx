c0c99ec09357ef9812ea3551cd573aa42bf365fd
IdentalerMaxima <koppany.naray@gmail.com>
2024-04-05 13:38:28 +0200

    css and fields in profile

--- vite-project/src/components/forms/PersonalData.jsx
+++ vite-project/src/components/forms/PersonalData.jsx
@@ -12,13 +12,13 @@ export default function PersonalData({ currentUser }) {
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
 
-          <div className="border-b border-gray-900/10 pb-1">
+          <div className="">
             <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
             <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
           </div>
 
 
-          <div className="mt-2 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
 
             <div className="sm:col-span-3">
               <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
@@ -54,7 +54,7 @@ export default function PersonalData({ currentUser }) {
               </div>
             </div>
 
-            <div className="sm:col-span-3">
+            <div className="sm:col-span-4">
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
               </label>
@@ -71,7 +71,7 @@ export default function PersonalData({ currentUser }) {
               </div>
             </div>
 
-            <div className="sm:col-span-3">
+            <div className="sm:col-span-2">
               <label htmlFor="phone" className="block text-sm font-medium leading-6 text-gray-900">
                 Phone
               </label>
@@ -88,7 +88,7 @@ export default function PersonalData({ currentUser }) {
               </div>
             </div>
 
-            <div className="sm:col-span-3">
+            <div className="sm:col-span-2 ">
               <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                 Birth date
               </label>
@@ -117,7 +117,38 @@ export default function PersonalData({ currentUser }) {
               </div>
             </div>
 
-            <div className="sm:col-span-3">
+            <div className="sm:col-span-2">
+              <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
+                Birth place
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  autoComplete="address-level2"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
+                Mothers name
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="city"
+                  id="city"
+                  autoComplete="address-level2"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+
+            {/* <div className="sm:col-span-3">
               <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
                 Country
               </label>
@@ -133,7 +164,7 @@ export default function PersonalData({ currentUser }) {
                   <option>Mexico</option>
                 </select>
               </div>
-            </div>
+            </div> */}
 
             <div className="col-span-full">
               <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
