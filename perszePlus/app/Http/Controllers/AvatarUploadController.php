62cf403f29428fe1a67b4d7c70359587edfe6cfd
IdentalerMaxima <koppany.naray@gmail.com>
2024-04-05 16:47:53 +0200

    add deleteOldAvatar function to backend

--- perszePlus/app/Http/Controllers/AvatarUploadController.php
+++ perszePlus/app/Http/Controllers/AvatarUploadController.php
@@ -17,28 +17,39 @@ class AvatarUploadController extends Controller
      */
     public function upload(AvatarUploadRequest $request)
     {
-        // Store the uploaded avatar file in the 'avatars' directory within the 'storage/app' directory
-        $path = $request->file('avatar')->store('public/avatars');
-
-        
-
-        // Update the authenticated user's avatar path in the database
+        // Get the authenticated user
         $user = Auth::user();
-        //Log::info($user);
+        //Log::info("User: " . $user->name);
+
+        $this->deleteOldAvatar($user);
+
+        // Store the uploaded avatar file in the 'avatars' directory within the 'storage/app' directory, also, laravel handles unique file names
+        $path = $request->file('avatar')->store('public/avatars');
+        //Log::info("Path to the stored avatar: " . $path);
 
         $avatarUrl = asset(Storage::url($path));
-        //Log::info($avatarUrl);
+        //Log::info("Avatar URL: " . $avatarUrl);
 
         $user->avatar_path = $avatarUrl;
-
-        
-
         $user->save();
-        //Log::info($user);
-
-        
 
         // Return a JSON response with the path to the stored avatar
         return response()->json(['path' => $avatarUrl], 200);
+    }
+
+    public function deleteOldAvatar($user)
+    {
+        $avatarPathFromDb = $user->avatar_path;
+        //Log::info("Avatar path from DB: " . $avatarPathFromDb);
+
+        $strippedAvatarPath = strstr($avatarPathFromDb, 'avatars');
+        //Log::info("Stripped avatar path: " . $strippedAvatarPath);
+
+        if ($strippedAvatarPath) {
+            //Log::info('File exists.');
+            Storage::disk('public')->delete($strippedAvatarPath);
+        } else {
+            //Log::info('File does not exist.');
+        }
     }
 }
