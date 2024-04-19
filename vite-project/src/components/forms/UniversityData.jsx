47182c21c9f47f24e602362da0cb9bccd40e8623
IdentalerMaxima <koppany.naray@gmail.com>
2024-04-19 14:31:36 +0200

    add list of universities fetched form db

--- vite-project/src/components/forms/UniversityData.jsx
+++ vite-project/src/components/forms/UniversityData.jsx
@@ -1,17 +1,36 @@
-
+import axiosClient from '../../axios';
+import { useEffect, useState } from 'react';
 
 export default function PersonalData() {
+  const [universities, setUniversities] = useState([]);
+
+  useEffect(() => {
+    // Fetch universities from the database
+    const fetchUniversities = async () => {
+      try {
+        const response = await axiosClient.get('/universities');
+        setUniversities(response.data); 
+        console.log('Universities:', data);
+      } catch (error) {
+        console.error('Error fetching universities:', error);
+      }
+    };
+
+    fetchUniversities();
+  }, []);
+
+
   return (
     <form>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">University data</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+          {/* <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p> */}
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-            <div className="sm:col-span-3">
+            <div className="sm:col-span-2">
               <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                First name
+                Neptun code
               </label>
               <div className="mt-2">
                 <input
@@ -24,24 +43,37 @@ export default function PersonalData() {
               </div>
             </div>
 
-            <div className="sm:col-span-3">
-              <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-                Last name
+            <div className="sm:col-span-2">
+              <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
+                University
               </label>
               <div className="mt-2">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+                <select
+                  id="university"
+                  name="university"
+                  autoComplete="country-name"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                >
+                  {/* <option>Pécsi Tudományegyetem</option>
+                  <option>Budapesti Corvinus Egyetem</option>
+                  <option>Budapesti Gazdasági Egyetem</option>
+                  <option>Debreceni Egyetem</option>
+                  <option>Eötvös Loránd Tudományegyetem</option>
+                  <option>Kaposvári Egyetem</option> */}
+
+                  {Array.isArray(universities) && universities.map((university) => (
+                    <option key={university.id} value={university.id}>
+                      {university.name}
+                    </option>
+                  ))}
+
+                </select>
               </div>
             </div>
 
-            <div className="sm:col-span-4">
+            <div className="sm:col-span-2">
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
+                Faculty
               </label>
               <div className="mt-2">
                 <input
@@ -134,7 +166,7 @@ export default function PersonalData() {
           </div>
         </div>
 
-        
+
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
