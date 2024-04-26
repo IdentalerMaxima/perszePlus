ffdcca9b56d7fad6fdd679e696efd620051bd705
IdentalerMaxima <koppany.naray@gmail.com>
2024-04-26 17:55:57 +0200

    documents migration

---
+++ perszePlus/database/migrations/2024_04_26_114020_create_documents_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('type');
+            $table->string('file_path');
+            $table->string('original_name');
+            $table->integer('size');
+            $table->timestamp('last_modified')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
