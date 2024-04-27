994e6b32956ef7972894c7b87674df5ec219c386
IdentalerMaxima <koppany.naray@gmail.com>
2024-04-27 22:42:44 +0200

    add documents fron adjustments

--- vite-project/src/components/forms/DocumentsData.jsx
+++ vite-project/src/components/forms/DocumentsData.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Fab } from '@mui/material';
+import { Fab, Button } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
+import DownloadIcon from '@mui/icons-material/Download';
 import CloseIcon from '@mui/icons-material/Close';
 import FileUpload from '../../components/upload/FileUpload';
 import axiosClient from '../../axios';
@@ -9,6 +11,7 @@ export default function DocumentsData() {
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [isUploadClicked, setIsUploadClicked] = useState(false);
   const [selectedDocument, setSelectedDocument] = useState(null);
+  const [checkboxedDocuments, setCheckboxedDocuments] = useState([]);
 
   const openDialog = () => {
     setIsUploadClicked(true);
@@ -32,20 +35,13 @@ export default function DocumentsData() {
     }
   };
 
-  const openDocument = (document) => {
-    setSelectedDocument(document);
-    console.log('Opening document:', document);
-  };
-
   const getFileExtension = (fileName) => {
     const parts = fileName.split('.');
     const extension = parts[parts.length - 1];
-    console.log('File extension:', extension);
     return extension;
   };
 
   const getFileIcon = (file) => {
-
     switch (getFileExtension(file.file_path)) {
       case 'jpeg':
       case 'png':
@@ -59,7 +55,7 @@ export default function DocumentsData() {
       case 'zip':
         return '../../src/assets/IconZIP.png';
       default:
-        return '../../src/assets/defaultFileIcon.jpg';
+        return '../../src/assets/IconZIP.png';
     }
   };
 
@@ -76,50 +72,140 @@ export default function DocumentsData() {
     return new Date(dateString).toLocaleDateString(undefined, options);
   };
 
+  const handleCheckboxChange = (file) => {
+    // Check if the file is already in the checkboxedDocuments array
+    const isChecked = checkboxedDocuments.some((doc) => doc.id === file.id);
+
+    if (isChecked) {
+
+      // If the file is already checked, remove it from the checkboxedDocuments array
+      setCheckboxedDocuments(
+        checkboxedDocuments.filter((doc) => doc.id !== file.id)
+      );
+
+    } else {
+
+      // If the file is not checked, add it to the checkboxedDocuments array
+      setCheckboxedDocuments([...checkboxedDocuments, file]);
+
+    }
+    
+    console.log(checkboxedDocuments);
+  };
+
+  const handleDeleteSelected = async () => {
+    try {
+      await axiosClient.delete('/delete/documents', {
+        data: { documents: checkboxedDocuments.map((doc) => doc.id) },
+      });
+      setCheckboxedDocuments([]);
+      fetchDocuments();
+    } catch (error) {
+      console.error('Error deleting documents:', error);
+    }
+  };
+
+  const checkboxAll = (event) => {
+    const isChecked = event.target.checked;
+    if (isChecked) {
+      setCheckboxedDocuments([...uploadedFiles]);
+      console.log(checkboxedDocuments);
+    } else {
+      setCheckboxedDocuments([]);
+    };
+  };
+
+
   return (
     <div className='h-full relative'>
-      <h2 className="text-base font-semibold leading-7 text-gray-900 mb-4">Uploaded Documents</h2>
+      <h2 className="text-base font-semibold leading-7 text-gray-900 ">Uploaded Documents</h2>
 
-      <div className="flex justify-between mb-4 pl-4">
-        <div className="text-gray-600 font-semibold w-1/5 flex justify-start">Name</div>
-        <div className="text-gray-600 font-semibold w-1/5 flex justify-start">Type</div>
-        <div className="text-gray-600 font-semibold w-1/5 flex justify-start">Upload date</div>
-        <div className="text-gray-600 font-semibold w-1/5 flex justify-start">Size</div>
-        <div className="text-gray-600 font-semibold w-1/5 flex justify-end"></div>
+      <div className='flex justify-end mb-10 space-x-4'>
+        <div className="">
+          <Button
+            variant="contained"
+            color="secondary"
+            disabled={checkboxedDocuments.length === 0}
+            onClick={handleDeleteSelected}
+            startIcon={<DeleteIcon />}
+          >
+            Delete
+            {/* {checkboxedDocuments.length== 0 ? '' : '(' + checkboxedDocuments.length + ')'} */}
+          </Button>
+        </div>
+
+        <div className="">
+          <Button
+            variant="contained"
+            color="primary"
+            disabled={checkboxedDocuments.length === 0}
+            onClick={handleDeleteSelected}
+            startIcon={<DownloadIcon />}
+          >
+            Download
+            {/* {checkboxedDocuments.length== 0 ? '' : '(' + checkboxedDocuments.length + ')'} */}
+          </Button>
+        </div>
+      </div>
+
+
+      <div className=''>
+
+      </div>
+      <div className="flex justify-between items-center">
+        <input
+          type="checkbox"
+          className="h-4 w-4 rounded-full border-gray-300 text-indigo-600"
+          onChange={(e) => checkboxAll(e)}
+        />
+        <div className="text-gray-600 font-semibold w-1/5 flex justify-start pl-20">Name</div>
+        <div className="text-gray-600 font-semibold w-2/5 flex justify-start">Type</div>
+        <div className="text-gray-600 font-semibold w-1/5 flex justify-start pl-4">Size</div>
+        <div className="text-gray-600 font-semibold w-1/5 flex justify-centre pl-4">Upload date</div>
       </div>
 
       <div className="mt-3 mb-24">
         {uploadedFiles.map((file, index) => (
 
-          <div key={index} className="flex justify-start items-center border-b py-2">
+          <div key={index} className="flex items-center border-b py-2">
 
-            <div className="w-1/5 flex justify-start px-4 cursor-pointer" onClick={() => openDocument(file)}>
-              <p className="font-semibold">{file.name}</p>
+            <div>
+              <input
+                type="checkbox"
+                className="h-4 w-4 rounded-full border-gray-300 text-indigo-600 pt-2"
+                onChange={() => handleCheckboxChange(file)}
+                checked={checkboxedDocuments.some((doc) => doc.id === file.id)}
+              />
             </div>
 
-            <div className="w-1/5 px-4">
+
+            <div className="w-1/5 flex justify-start px-8">
+              <img
+                src={getFileIcon(file)}
+                alt="file icon"
+                className="w-6 h-6"
+              />
+              <p className="font-semibold pl-6">{file.name}</p>
+            </div>
+
+            <div className="w-2/5 ">
               <p>{file.type}</p>
-            </div>
-
-            <div className="w-1/5 px-4">
-              <p>{formatDate(file.last_modified)}</p>
             </div>
 
             <div className="w-1/5 px-4">
               <p>{formatSize(file.size)}</p>
             </div>
 
-            <div className="w-1/5 px-4 flex justify-end">
-              <img
-                src={getFileIcon(file)}
-                alt="file icon"
-                className="w-6 h-6 cursor-pointer"
-                onClick={() => openDocument(file)}
-              />
+            <div className="w-1/5 pl-4 justify-end">
+              <p>{formatDate(file.last_modified)}</p>
             </div>
+
+
           </div>
         ))}
       </div>
+
+
 
       {!selectedDocument && (
         <div className="absolute bottom-4 right-4">
