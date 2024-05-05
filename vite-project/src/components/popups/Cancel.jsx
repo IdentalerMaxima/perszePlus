f31c393e43060262508000302450332be07485a7
IdentalerMaxima <koppany.naray@gmail.com>
2024-05-05 23:18:35 +0200

    create cancel popup

---
+++ vite-project/src/components/popups/Cancel.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Dialog, Button, Typography, TextField, DialogContent } from "@mui/material";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function Cancel({ handleClose, handleRerender}) {
+
+    const closeDialog = () => {
+        handleClose();
+    }
+
+    const cancelOperation = () => {
+        handleRerender();
+        closeDialog();
+    }
+
+        return (
+            <Dialog open={true} onClose={handleClose} className="w-">
+                <DialogTitle>Cancel?</DialogTitle>
+                <DialogContent
+                    className="flex flex-col"
+                >
+                    <div>
+                        Changes will not be saved. Are you sure you want to cancel?
+                    </div>
+
+                    <div className="flex w-full mt-4 justify-end">
+                        <Button
+                            onClick={closeDialog}
+                            color="secondary"
+                        >
+                            No
+                        </Button>
+                        <Button
+                            color="primary"
+                            onClick={cancelOperation}
+                        >
+                            Yes 
+                        </Button>
+                    </div>
+                </DialogContent>
+            </Dialog>
+        )
+    }
