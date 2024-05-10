4db17173a3b4af14ef152f1c661301e7f906c0a8
IdentalerMaxima <koppany.naray@gmail.com>
2024-05-10 22:47:57 +0200

    birth date migrations

---
+++ perszePlus/database/migrations/2024_05_10_195445_rename_place_of_birth.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('place_of_birth', 'birth_place');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('birth_place', 'place_of_birth');
+        });
+    }
+};
