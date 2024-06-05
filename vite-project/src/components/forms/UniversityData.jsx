0cd1408369dfd0afad30bbed0d5a017d1ea8f9ac
IdentalerMaxima <koppany.naray@gmail.com>
2024-06-05 17:51:37 +0200

    fix university problems

--- vite-project/src/components/forms/UniversityData.jsx
+++ vite-project/src/components/forms/UniversityData.jsx
@@ -1,19 +1,39 @@
 import axiosClient from '../../axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState,} from 'react';
+import Cancel from '../popups/Cancel';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 export default function PersonalData() {
+  const { currentUser, setCurrentUser } = useStateContext();
+  const initialFormData =
+  {
+    neptun_code: currentUser.neptun_code ? currentUser.neptun_code : '',
+    university: currentUser.university ? currentUser.university : '',
+    faculty: currentUser.faculty ? currentUser.faculty : '',
+    start_year: currentUser.start_year ? currentUser.start_year : '',
+    current_semester: currentUser.current_semester ? currentUser.current_semester : '',
+    educational_format: currentUser.educational_format ? currentUser.educational_format : '',
+    level_of_education: currentUser.level_of_education ? currentUser.level_of_education : '',
+  }
+  const [formData, setFormData] = useState(initialFormData);
   const [universities, setUniversities] = useState([]);
   const [selectedUniversity, setSelectedUniversity] = useState(null);
+  const [universitiesLoading, setUniversitiesLoading] = useState(true);
   const [faculties, setFaculties] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [isCancelClicked, setIsCancelClicked] = useState(false);
+  const [rerender, setRerender] = useState(false);
 
+  // Fetch universities on mount
   useEffect(() => {
-    // Fetch universities from the database
     const fetchUniversities = async () => {
       try {
         const response = await axiosClient.get('/universities');
         setUniversities(response.data);
-        //console.log('Universities:', data);
+        setUniversitiesLoading(false);
       } catch (error) {
+        setUniversitiesLoading(false);
         console.error('Error fetching universities:', error);
       }
     };
@@ -21,29 +41,91 @@ export default function PersonalData() {
     fetchUniversities();
   }, []);
 
+  //Set selected university if user has one and fetch faculties
+  useEffect(() => {
+    if (!universitiesLoading && formData.university) {
+      const selectedUniv = universities.find(university => university.name === formData.university);
+      if (selectedUniv) {
+        setSelectedUniversity(selectedUniv);
+        fetchFaculties(selectedUniv.id);
+      }
+    }
+  }, [universitiesLoading, universities, formData.university]);
+
+
   const fetchFaculties = async (universityId) => {
     try {
       const response = await axiosClient.get(`/universities/${universityId}/faculties`);
       setFaculties(response.data);
-      //console.log('Faculties:', response.data);
     } catch (error) {
-      console.error('Error fetching faculties:', error);
+      console.error('Error fetching faculties: ', error);
     }
   }
 
   const handleUniversityChange = (event) => {
-    const universityId = event.target.value;
-    setSelectedUniversity(universityId);
-    fetchFaculties(universityId);
+    const universityId = parseInt(event.target.value);
+    const newSelectedUniversity = universities.find(university => university.id === universityId);
+    if (newSelectedUniversity) {
+      setSelectedUniversity(newSelectedUniversity);
+      setFormData({
+        ...formData,
+        university: newSelectedUniversity.name,
+        faculty: '',
+      });
+      fetchFaculties(newSelectedUniversity.id);
+    } else {
+      console.log('No university selected');
+    }
   };
 
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError('');
+    try {
+      console.log('Data to save:', formData);
+      const response = await axiosClient.post('/user/info', formData);
+      setLoading(false);
+
+      //retrieve new currentUser data
+      const newUserData = await axiosClient.get('/user/info');
+      setCurrentUser(newUserData.data.user);
+
+    } catch (error) {
+      setError('An error occurred while saving data.');
+      setLoading(false);
+    }
+  };
+
+  const handleCancel = () => {
+    console.log('Cancel clicked');
+    openCancelDialog();
+  };
+
+  const openCancelDialog = () => {
+    setIsCancelClicked(true);
+  };
+
+  const resetFormFields = () => {
+    setFormData(initialFormData);
+    setRerender(prev => !prev);
+  }
+
+
   return (
-    <form>
+    <form key={rerender}>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">University data</h2>
-          {/* <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p> */}
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-2">
@@ -52,11 +134,12 @@ export default function PersonalData() {
               </label>
               <div className="mt-2">
                 <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  name="neptun_code"
+                  id="neptun-code"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                   focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={formData.neptun_code}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -67,26 +150,21 @@ export default function PersonalData() {
               </label>
               <div className="mt-2">
                 <select
-                  id="university"
+                  id="university-name"
                   name="university"
-                  autoComplete="country-name"
                   onChange={handleUniversityChange}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                >
-                  {/* <option>Pécsi Tudományegyetem</option>
-                  <option>Budapesti Corvinus Egyetem</option>
-                  <option>Budapesti Gazdasági Egyetem</option>
-                  <option>Debreceni Egyetem</option>
-                  <option>Eötvös Loránd Tudományegyetem</option>
-                  <option>Kaposvári Egyetem</option> */}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset
+                   focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  value={selectedUniversity ? selectedUniversity.id : ''}
+                  //handleChange={handleChange}
 
+                >
                   {!selectedUniversity && <option value="">Select a university</option>}
                   {Array.isArray(universities) && universities.map((university) => (
                     <option key={university.id} value={university.id}>
                       {university.name}
                     </option>
                   ))}
-
                 </select>
               </div>
             </div>
@@ -97,16 +175,22 @@ export default function PersonalData() {
               </label>
               <div className="mt-2">
                 <select
-                  id="faculty"
+                  id="faculty-name"
                   name="faculty"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset
+                   focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  value={formData.faculty}
+                  onChange={handleChange}
+                  disabled={!formData.university}
                 >
-                  {/* <option>Pécsi Tudományegyetem</option>
-                  <option>Budapesti Corvinus Egyetem</option>
-                  <option>Budapesti Gazdasági Egyetem</option>
-                  <option>Debreceni Egyetem</option>
-                  <option>Eötvös Loránd Tudományegyetem</option>
-                  <option>Kaposvári Egyetem</option> */}
+
+                  {!formData.university && (
+                    <option value="" disabled>Select a university first!</option>
+                  )}
+
+                  {!formData.faculty && (
+                    <option value="" >Select a faculty</option>
+                  )}
 
                   {Array.isArray(faculties) && faculties.map((faculty) => (
                     <option key={faculty.id} value={faculty.id}>
@@ -125,10 +209,12 @@ export default function PersonalData() {
               <div className="mt-2">
                 <input
                   type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  name="start_year"
+                  id="start-year"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                   focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  defaultValue={formData.start_year}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -139,11 +225,12 @@ export default function PersonalData() {
               </label>
               <div className="mt-2">
                 <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  name="current_semester"
+                  id="current-semester"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                   focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  value={formData.current_semester}
+                  onChange={handleChange}
                 />
               </div>
             </div>
@@ -154,13 +241,23 @@ export default function PersonalData() {
               </label>
               <div className="mt-2">
                 <select
-                  id="faculty"
-                  name="faculty"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  id="educational-format"
+                  name="educational_format"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset
+                   focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  value={formData.educational_format}
+
+                  onChange={handleChange}
                 >
-                  <option>Full-time education</option>
-                  <option>Evening education</option>
-                  <option> Correspondence learning</option>
+
+                  {!formData.educational_format && (
+                    <option value="">Select an educational format</option>
+                  )}
+                  {['Full-time', 'Part-time', 'Distance learning'].map((format) => (
+                    <option key={format} value={format}>
+                      {format}
+                    </option>
+                  ))}
 
                 </select>
               </div>
@@ -172,40 +269,57 @@ export default function PersonalData() {
               </label>
               <div className="mt-2">
                 <select
-                  id="faculty"
-                  name="faculty"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  id="level-of-education"
+                  name="level_of_education"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset
+                   focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  value={formData.level_of_education}
+                  onChange={handleChange}
                 >
-                  <option>BSc</option>
-                  <option>BA</option>
-                  <option>PhD</option>
-                  <option>MSc</option>
-                  <option>MA</option>
-                  <option>Other</option>
+
+                  {!formData.level_of_education && (
+                    <option value="">Select a level of education</option>
+                  )}
+                  {['BSc', 'BA', 'PhD', 'MSc', 'MA', 'Other'].map((level) => (
+                    <option key={level} value={level}>
+                      {level}
+                    </option>
+                  ))}
 
                 </select>
               </div>
             </div>
 
           </div>
-
-
-
-          
-
         </div>
         <div className="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Save
-            </button>
-          </div>
+          <button
+            type="button"
+            className="text-sm font-semibold leading-6 text-gray-900"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline
+               focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={handleSubmit}
+          >
+            Save
+          </button>
+        </div>
       </div>
+
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+
+      {isCancelClicked && (
+        <Cancel
+          handleClose={() => setIsCancelClicked(false)}
+          resetForm={resetFormFields}
+        />
+      )}
 
     </form>
   )
