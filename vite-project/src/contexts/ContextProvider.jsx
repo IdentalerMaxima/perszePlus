a062f19007923de9a202b79cf220a9a4da146a4d
IdentalerMaxima <koppany.naray@gmail.com>
2024-07-12 10:39:39 +0200

    check if user is auth before calling for data

--- vite-project/src/contexts/ContextProvider.jsx
+++ vite-project/src/contexts/ContextProvider.jsx
@@ -11,18 +11,35 @@ const StateContext = createContext({
 
 export const ContextProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState({});
-    const [userToken, _setUserToken] = useState(localStorage.getItem('TOKEN') || '');
+    const [userToken, _setUserToken] = useState(localStorage.getItem('TOKEN') || null);
     const [isAdmin, setIsAdmin] = useState(false);
 
     const fetchUserData = async () => {
+        // Check if userToken exists
+        if (!userToken) {
+            setCurrentUser({});
+            setIsAdmin(false);
+            return; // Exit if not authorized
+        }
+    
         try {
             const response = await axiosClient.get('/user/info');
-            setCurrentUser(response.data.user);
-            setIsAdmin(response.data.user.category === 'admin' || response.data.user.category === 'vezetőség');
+    
+            if (response.status === 200) {
+                setCurrentUser(response.data.user);
+                setIsAdmin(response.data.user.category === 'admin' || response.data.user.category === 'vezetőség');
+            }
         } catch (error) {
-            console.error(error);
+            console.error('Error fetching user data:', error);
+    
+            // Handle unauthorized access
+            if (error.response && error.response.status === 401) {
+                setUserToken(null);
+                setCurrentUser({});
+            }
         }
     };
+    
 
     useEffect(() => {
         fetchUserData();
