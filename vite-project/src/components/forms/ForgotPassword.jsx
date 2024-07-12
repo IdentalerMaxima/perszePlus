8274715a965bfa3fdeddd824e26c3b2f3912c4d7
IdentalerMaxima <koppany.naray@gmail.com>
2024-07-12 09:09:59 +0200

    edit forgot pword front

--- vite-project/src/components/forms/ForgotPassword.jsx
+++ vite-project/src/components/forms/ForgotPassword.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Container, Alert } from '@mui/material';
 import axiosClient from '../../axios';
+import { useTranslation } from "react-i18next";
 
 const ForgotPassword = () => {
+    const { t } = useTranslation(['translation']);
+
     const [email, setEmail] = useState('');
     const [successMessage, setSuccessMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
@@ -11,6 +14,7 @@ const ForgotPassword = () => {
         e.preventDefault();
         try {
             const response = await axiosClient.post('/forgotPassword', { email });
+            console.log(response.data);
             setSuccessMessage(response.data.message);
             setErrorMessage('');
         } catch (error) {
@@ -20,28 +24,55 @@ const ForgotPassword = () => {
     };
 
     return (
-        <Container maxWidth="sm">
-            <Typography variant="h4" align="center" gutterBottom>
-                Forgot Password
-            </Typography>
-            <form onSubmit={handleSubmit}>
-                <TextField
-                    label="Email Address"
-                    variant="outlined"
-                    fullWidth
-                    margin="normal"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                />
-                <Button variant="contained" color="primary" type="submit" fullWidth>
-                    Send Reset Link
-                </Button>
-            </form>
-            {successMessage && <Alert severity="success">{successMessage}</Alert>}
-            {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
-        </Container>
-    );
+        <>
+            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                {'Email to reset password'}
+            </h2>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
+                {successMessage && (
+                    <Alert severity="success" onClose={() => setSuccessMessage('')}>
+                        {successMessage}
+                    </Alert>
+                )}
+                {errorMessage && (
+                    <Alert severity="error" onClose={() => setErrorMessage('')}>
+                        {errorMessage}
+                    </Alert>
+                )}
+
+                <form onSubmit={handleSubmit} className="space-y-6" action="#" method="POST">
+                    <div>
+                        <div className="mt-14">
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                value={email}
+                                onChange={(ev) => setEmail(ev.target.value)}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                                 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder={t("type your email")}
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm
+                             hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            {'Reset Password'}
+                        </button>
+                    </div>
+                </form>
+
+            </div>
+        </>
+    )
 };
 
 export default ForgotPassword;
