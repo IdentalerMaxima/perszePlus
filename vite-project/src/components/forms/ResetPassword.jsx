44ab4827b592bfe2a466dc2c9bb3f605b3151b38
IdentalerMaxima <koppany.naray@gmail.com>
2024-07-12 17:32:11 +0200

    password setting errors

--- vite-project/src/components/forms/ResetPassword.jsx
+++ vite-project/src/components/forms/ResetPassword.jsx
@@ -1,89 +1,106 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { CircularProgress } from '@mui/material';
 import axiosClient from '../../axios';
+import { useTranslation } from 'react-i18next';
 
 const PasswordResetForm = () => {
     const navigate = useNavigate();
+    const { token } = useParams();
+    const resetEmail = localStorage.getItem('reset_email');
+    const { t } = useTranslation(['translation']);
+
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
+    const [error, setError] = useState({ __html: '' });
     const [submitted, setSubmitted] = useState(false);
 
-    const { token } = useParams();
-    const resetEmail = localStorage.getItem('reset_email');
+    useEffect(() => {
+        setError({ __html: '' });
+    }, []);
 
-    const handleSubmit = (event) => {
+    const translateErrors = (errorMessages) => {
+        const errors = errorMessages.split('<br>');
+        const translatedErrors = errors.map(errorMessage => t(errorMessage));
+        setError({ __html: translatedErrors.join('<br>') });
+    };
+
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
-        console.log('token', token);
-
         if (password !== confirmPassword) {
-            setError('Passwords do not match');
+            setError({ __html: 'Passwords do not match' });
             return;
         }
 
-        try {
-            setLoading(true);
-            setError('');
+        setLoading(true);
+        setError({ __html: '' });
 
-            // Call your API here
-            axiosClient.post('resetPassword', {
+        try {
+            const response = await axiosClient.post('resetPassword', {
                 token,
-                email : resetEmail,
+                email: resetEmail,
                 password,
                 password_confirmation: confirmPassword,
             });
-            setSubmitted(true);
-            setTimeout(() => {
-                navigate('/login');
-            }, 3000);
 
-        }
-        catch (error) {
-            console.error(error);
-            setError('An error occurred');
+            if (response.status === 200) {
+                setSubmitted(true);
+                setTimeout(() => {
+                    navigate('/login');
+                }, 3000);
+            }
+        } catch (error) {
+            if (error.response && error.response.data.errors) {
+                const finalErrors = Object.values(error.response.data.errors)
+                    .reduce((accum, next) => [...accum, ...next], [])
+                    .join('<br>');
+                translateErrors(finalErrors);
+            } else {
+                setError({ __html: 'An unexpected error occurred' });
+            }
             setLoading(false);
         }
-
     };
 
     return (
         <>
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                {'Set New Password'}
+                {'Set a new password'}
             </h2>
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                {error.__html && (
+                    <div className="bg-red-500 rounded py-2 px-3 text-white"
+                        dangerouslySetInnerHTML={error}>
+                    </div>
+                )}
+
                 {!submitted ? (
-                    <form onSubmit={handleSubmit} className="space-y-6" action="#" method="POST">
-                        <div>
-                            <div className="mt-14">
-                                <input
-                                    id="password"
-                                    name="password"
-                                    type="password"
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    placeholder={'New Password'}
-                                />
-                            </div>
+                    <form onSubmit={handleSubmit} className="space-y-6">
+                        <div className="mt-14">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="block w-full rounded-md border-gray-300 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder={'New password'}
+                            />
                         </div>
 
-                        <div>
-                            <div className="">
-                                <input
-                                    id="confirmPassword"
-                                    name="confirmPassword"
-                                    type="password"
-                                    value={confirmPassword}
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
-                                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                    placeholder={'Confirm Password'}
-                                />
-                            </div>
+                        <div className="pb-4">
+                            <input
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                type="password"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                className="block w-full rounded-md border-gray-300 py-1.5 text-gray-900 shadow-sm placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder={'Confirm new password'}
+                            />
                         </div>
 
                         <div>
@@ -94,11 +111,10 @@ const PasswordResetForm = () => {
                                 {loading ? <CircularProgress size={24} color="inherit" /> : 'Reset Password'}
                             </button>
                         </div>
-                        {error && <div className="text-red-500 text-center">{error}</div>}
                     </form>
                 ) : (
                     <div className="text-green-500 text-center">
-                        Password reset successfully! Redirecting to login...
+                        {t('password reset successfully! Redirecting to login...')}
                     </div>
                 )}
             </div>
