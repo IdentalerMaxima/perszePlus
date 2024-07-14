61842bbe24db983626394a621c8dd732554896fe
IdentalerMaxima <koppany.naray@gmail.com>
2024-07-14 13:29:26 +0200

    create post factorz and seeder

---
+++ perszePlus/database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'author_id' => User::factory(),
+            'content' => $this->faker->text,
+            'likes' => $this->faker->numberBetween(0, 100),
+        ];
+    }
+}
