191dcce0c811797d7f2d316a858b10b696b8fb3e
IdentalerMaxima <koppany.naray@gmail.com>
2024-07-14 15:48:19 +0200

    add post relationship to user model

--- perszePlus/app/Models/User.php
+++ perszePlus/app/Models/User.php
@@ -69,4 +69,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany(Event::class)->withPivot('status')->withTimestamps();
     }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class, 'author_id');
+    }
 }
