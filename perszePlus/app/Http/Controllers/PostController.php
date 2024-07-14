c4df0f60bed0a66a12f09d94b0dec5dae78cf960
IdentalerMaxima <koppany.naray@gmail.com>
2024-07-14 15:47:51 +0200

    edit post controller response

--- perszePlus/app/Http/Controllers/PostController.php
+++ perszePlus/app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
+use Illuminate\Support\Facades\Log;
 
 class PostController extends Controller
 {
@@ -11,7 +13,35 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        // Fetch posts with their related author and comments
+        $posts = Post::with(['author', 'comments'])->get();
+
+        // Structure the response to include the author's name and avatar path, and the comments
+        $posts = $posts->map(function ($post) {
+            return [
+                'id' => $post->id,
+                'content' => $post->content,
+                'likes' => $post->likes,
+                'date' => $post->created_at->toDateTimeString(),
+                'author' => [
+                    'first_name' => $post->author->first_name,
+                    'last_name' => $post->author->last_name,
+                    'avatar_path' => $post->author->avatar_path,
+                ],
+                'comments' => $post->comments->map(function ($comment) {
+                    return [
+                        'user' => $comment->author->first_name . ' ' . $comment->author->last_name,
+                        'comment' => $comment->content,
+                        'avatar_path' => $comment->author->avatar_path,
+                    ];
+                }),
+            ];
+        });
+
+        // Log the posts
+        Log::info('Posts fetched successfully', ['posts' => $posts]);
+
+        return response()->json($posts, 200);
     }
 
     /**
