770d1b91327187aea815cca0c24bac50da75f746
IdentalerMaxima <koppany.naray@gmail.com>
2024-07-19 17:29:33 +0200

    add store fn to course c

--- perszePlus/app/Http/Controllers/CourseController.php
+++ perszePlus/app/Http/Controllers/CourseController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Course;
+use Storage;
+use Log;
 
 
 class CourseController extends Controller
@@ -14,4 +16,44 @@ class CourseController extends Controller
 
         return response()->json($courses, 200);
     }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'image|mimes:jpeg,jpg,png|max:2048',
+            'semester' => 'required|string',
+            'recommended_year' => 'required|string',
+            'host' => 'required|string',
+            'requirements' => 'required|string',
+            'dates' => 'required|string',
+        ]);
+
+        $defaultImage = 'public/images/default_banner.jpg';
+    
+        if ($request->hasFile('image')) {
+            $image = $request->file('image')->store('public/images');
+            $imagePath = asset(Storage::url($image));
+        } else if ($defaultImage) {
+            $imagePath = asset(Storage::url($defaultImage));
+        } else {
+            $imagePath = null;
+        }
+    
+        // Create course record
+        $course = Course::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image_path' => $imagePath,
+            'semester' => $request->semester,
+            'recommended_year' => $request->recommended_year,
+            'host' => $request->host,
+            'requirements' => $request->requirements,
+            'dates' => $request->dates,
+        ]);
+    
+        return response()->json($course, 201);
+    }
+    
 }
