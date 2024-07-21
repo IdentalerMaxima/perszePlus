2ee14736317c6ffc42f3a75d832947a2024e4688
IdentalerMaxima <koppany.naray@gmail.com>
2024-07-21 20:55:26 +0200

    add get docs and view logic to doc controller

--- perszePlus/app/Http/Controllers/DocumentController.php
+++ perszePlus/app/Http/Controllers/DocumentController.php
@@ -13,9 +13,7 @@ class DocumentController extends Controller
     public function index(Request $request)
     {
         $userId = $request->user()->id;
-        // Log::info($userId);
         $documents = Document::where('user_id', $userId)->get();
-        //Log::info($documents);
         return response()->json(['documents' => $documents]);
     }
 
@@ -36,27 +34,16 @@ class DocumentController extends Controller
         $fileName = $request->input('name');
         $fileType = $request->input('type');
 
-        // Log::info($request);
-        // Log::info($file);
-        // Log::info($fileName);
-        // Log::info($fileType);
-        //Log::info($userId);
-
         $directory = "uploads/{$userId}";
         if (!Storage::exists($directory)) {
             Storage::makeDirectory($directory);
         }
 
-        //Log::info($directory);
-
-        // Store file in the storage directory
         $path = uniqid() . '_' . $file->getClientOriginalName();
         $file->storeAs($directory, $path);
 
-
-        // Save metadata record of the document
         $document = new Document();
-        $document->user_id = auth()->user()->id; // Assuming you are using authentication
+        $document->user_id = auth()->user()->id; 
         $document->name = $fileName;
         $document->type = $fileType;
         $document->original_name = $file->getClientOriginalName();
@@ -120,11 +107,37 @@ class DocumentController extends Controller
             ->groupBy('type')
             ->get();
 
-        // Log the retrieved data for debugging or informational purposes
-        // Log::info($usersByDocumentType);
-
         return response()->json($usersByDocumentType);
     }
+
+    public function getDocumentsOfUser(Request $request, $userId)
+    {
+        $documents = Document::where('user_id', $userId)->get();
+        return response()->json(['documents' => $documents]);
+    }
+
+    public function show(Request $request, $fileId)
+    {
+        $userId = $request->user()->id;
+
+        if ($request->user()->category == 'admin') {
+            $document = Document::find($fileId);
+        } else {
+            $document = Document::where('user_id', $userId)->where('id', $fileId)->first();
+        }
+
+        if (!$document) {
+            return response()->json(['message' => 'File not found'], 404);
+        }
+
+        $filePath = storage_path('app/' . $document->file_path);
+
+        if (!file_exists($filePath)) {
+            return response()->json(['message' => 'File not found'], 404);
+        }
+        return response()->file($filePath);
+    }
+
 
 
 
