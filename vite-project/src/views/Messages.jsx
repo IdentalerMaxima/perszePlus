a03f41881205914c1b0c6e09ac86210a9e4754df
IdentalerMaxima <koppany.naray@gmail.com>
2024-08-08 18:48:40 +0200

    fix mobile view to see messages

--- vite-project/src/views/Messages.jsx
+++ vite-project/src/views/Messages.jsx
@@ -3,7 +3,6 @@ import PageComponent from "../components/PageComponent";
 import {
     Avatar,
     Box,
-    Container,
     Grid,
     List,
     ListItemAvatar,
@@ -14,17 +13,19 @@ import {
     Stack,
     Menu,
     MenuItem,
-    Button
+    Button,
+    Container
 } from "@mui/material";
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import AddIcon from '@mui/icons-material/Add';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import axiosClient from "../axios";
 import { useStateContext } from "../contexts/ContextProvider";
 import WriteMessage from "../components/forms/WriteMessage";
+import { useMediaQuery, useTheme } from '@mui/material';
 
 export default function Messages() {
     const { selectedMessageId, setSelectedMessageId, messages, setMessages } = useStateContext();
-    
     const [messageLoaded, setMessageLoaded] = useState(null);
     const [currentPage, setCurrentPage] = useState(1);
     const [anchorEl, setAnchorEl] = useState(null);
@@ -32,13 +33,15 @@ export default function Messages() {
     const [newMessage, setNewMessage] = useState("");
     const messagesPerPage = 7;
 
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
     useEffect(() => {
-        console.log("Messages loaded:", messages);
-        if (messages.length > 0) {
+        if (messages.length > 0 && !selectedMessageId && !isMobile) {
             setMessageLoaded(messages[0]);
             setSelectedMessageId(messages[0].id);
         }
-    }, [messages]);
+    }, [messages, selectedMessageId]);
 
     const handleMenuClick = (event, message) => {
         event.stopPropagation();
@@ -55,13 +58,16 @@ export default function Messages() {
 
         try {
             await axiosClient.delete(`/messages/${selectedMessageId}`);
-            setMessages(messages.filter(message => message.id !== selectedMessageId));
+            const newMessages = messages.filter(message => message.id !== selectedMessageId);
+            setMessages(newMessages);
             if (messageLoaded?.id === selectedMessageId) {
-                const newLoadedMessage = messages[0] ? messages[0] : null;
+                const newLoadedMessage = newMessages[0] ? newMessages[0] : null;
                 setMessageLoaded(newLoadedMessage);
                 setSelectedMessageId(newLoadedMessage ? newLoadedMessage.id : null);
             }
-            setSelectedMessageId(null);
+            if (!newMessages.length) {
+                setSelectedMessageId(null);
+            }
         } catch (error) {
             console.error(error);
         }
@@ -103,7 +109,7 @@ export default function Messages() {
 
     const handleSendMessage = async (recipientId) => {
         try {
-            const response = await axiosClient.post('/messages', {
+            await axiosClient.post('/messages', {
                 message: newMessage,
                 recipientId: recipientId,
             });
@@ -117,81 +123,102 @@ export default function Messages() {
 
     return (
         <PageComponent title="Messages">
-            <Box className="pt-1 bg-slate-100 rounded-md overflow-hidden" sx={{ height: '68vh' }}>
-                <Grid container spacing={3} className="h-full">
+            <Box className="pt-1 bg-slate-100 rounded-md overflow-hidden" sx={{ height: { xs: 'auto', md: '700px' } }}>
+                <Grid container spacing={2} className="h-full">
                     {/* List of messages */}
-                    <Grid item xs={12} md={4}>
-                        <Box className="border-r-2 overflow-y-auto h-full flex flex-col">
-                            <Box className="p-4">
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                    startIcon={<AddIcon />}
-                                    onClick={handleNewMessageOpen}
-                                    className="w-full"
-                                >
-                                    Write Message
-                                </Button>
-                            </Box>
-                            <List>
-                                {currentMessages.map((message) => (
-                                    <ListItemButton
-                                        key={message.id}
-                                        alignItems="flex-start"
-                                        className={`mb-1 border ${message.read ? 'border-gray-400' : 'border-red-400'}`}
-                                        onClick={() => loadMessage(message)}
+                    {(!selectedMessageId || !isMobile) && (
+                        <Grid item xs={12} md={4} sx={{ height: { xs: 'auto', md: '100%' }, overflowY: 'auto' }}>
+                            <Box className="border-r-2 flex flex-col">
+                                <Box className="p-4">
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        startIcon={<AddIcon />}
+                                        onClick={handleNewMessageOpen}
+                                        fullWidth
                                     >
-                                        <ListItemAvatar>
-                                            <Avatar alt={message.senderName} src={message.senderAvatar} />
-                                        </ListItemAvatar>
-                                        <ListItemText
-                                            primary={message.senderName}
-                                            secondary={
-                                                <Box className={`max-w-full overflow-hidden text-ellipsis whitespace-nowrap ${!message.read ? 'font-bold' : ''}`}>
-                                                    {message.message}
-                                                </Box>
-                                            }
-                                        />
-                                        <IconButton
-                                            edge="end"
-                                            aria-label="more"
-                                            onClick={(e) => handleMenuClick(e, message)}
+                                        Write Message
+                                    </Button>
+                                </Box>
+                                <List>
+                                    {currentMessages.map((message) => (
+                                        <ListItemButton
+                                            key={message.id}
+                                            alignItems="flex-start"
+                                            className={`mb-1 border ${message.read ? 'border-gray-400' : 'border-red-400'}`}
+                                            onClick={() => loadMessage(message)}
                                         >
-                                            <MoreVertIcon />
-                                        </IconButton>
-                                    </ListItemButton>
-                                ))}
-                            </List>
-                            <Stack direction="row" spacing={2} className="justify-center p-4 flex mt-auto">
-                                <Pagination
-                                    count={totalPages}
-                                    page={currentPage}
-                                    onChange={(e, page) => setCurrentPage(page)}
-                                />
-                            </Stack>
-                        </Box>
-                    </Grid>
+                                            <ListItemAvatar>
+                                                <Avatar alt={message.senderName} src={message.senderAvatar} />
+                                            </ListItemAvatar>
+                                            <ListItemText
+                                                primary={message.senderName}
+                                                secondary={
+                                                    <Box className={`max-w-full overflow-hidden text-ellipsis whitespace-nowrap ${!message.read ? 'font-bold' : ''}`}>
+                                                        {message.message}
+                                                    </Box>
+                                                }
+                                            />
+                                            <IconButton
+                                                edge="end"
+                                                aria-label="more"
+                                                onClick={(e) => handleMenuClick(e, message)}
+                                            >
+                                                <MoreVertIcon />
+                                            </IconButton>
+                                        </ListItemButton>
+                                    ))}
+                                </List>
+                                <Stack direction="row" spacing={2} className="justify-center p-4 flex mt-auto">
+                                    <Pagination
+                                        count={totalPages}
+                                        page={currentPage}
+                                        onChange={(e, page) => setCurrentPage(page)}
+                                    />
+                                </Stack>
+                            </Box>
+                        </Grid>
+                    )}
 
                     {/* Detailed Message */}
-                    <Grid item xs={12} md={8}>
-                        <Container className="p-4 flex flex-col justify-start items-end">
-                            {messageLoaded ? (
-                                <>
-                                    <Box className="flex items-center space-x-4">
-                                        <Avatar src={messageLoaded.senderAvatar} alt={messageLoaded.senderName}></Avatar>
-                                        <h3 className="text-2xl font-bold">{messageLoaded.senderName}</h3>
+                    {(selectedMessageId || isMobile) && (
+                        <Grid item xs={12} md={8}>
+                            <Container className="p-4 flex flex-col" sx={{ height: { xs: 'auto', md: '100%' }, overflowY: 'auto' }}>
+                                {selectedMessageId && isMobile && (
+                                    <Box>
+                                        <Box className="flex items-center">
+                                        <IconButton
+                                            onClick={() => {
+                                                setSelectedMessageId(null);
+                                                setMessageLoaded(null);
+                                            }}
+                                            sx={{ marginRight: 2 }}
+                                        >
+                                            <ArrowBackIcon />
+                                        </IconButton>
+                                        {messageLoaded && (
+                                            <Box className="flex items-center space-x-4">
+                                                <Avatar src={messageLoaded.senderAvatar} alt={messageLoaded.senderName} />
+                                                <h3 className="text-2xl font-bold">{messageLoaded.senderName}</h3>
+                                            </Box>
+                                        )} 
                                     </Box>
-                                    <Box className="mt-6 flex-grow overflow-y-auto">
-                                        <p>{messageLoaded.message}</p>
+                                    <Container className="p-6">
+                                        {messageLoaded.message}
+                                    </Container>
                                     </Box>
-                                </>
-                            ) : (
-                                <Box className="mt-6 flex-grow overflow-y-auto">
-                                    <p>No message selected</p>
-                                </Box>
-                            )}
-                        </Container>
-                    </Grid>
+                                )}
+                                {messageLoaded && !isMobile && (
+                                    <>
+                                        <Box className="flex items-center space-x-4">
+                                            <Avatar src={messageLoaded.senderAvatar} alt={messageLoaded.senderName} />
+                                            <h3 className="text-2xl font-bold">{messageLoaded.senderName}</h3>
+                                        </Box>
+                                    </>
+                                )}
+                            </Container>
+                        </Grid>
+                    )}
                 </Grid>
 
                 {/* Menu for delete option */}
