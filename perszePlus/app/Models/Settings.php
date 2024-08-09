5a5d0d213cd89048b95f6734adf8bc0818056de8
IdentalerMaxima <koppany.naray@gmail.com>
2024-08-09 18:59:04 +0200

    feat: Add Settings model for user settings management

---
+++ perszePlus/app/Models/Settings.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Settings extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'receive_notification_new_event',
+        'receive_notification_new_post',
+        'receive_notification_new_course',
+        'receive_email_notifications'
+    ];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    
+
+
+}
