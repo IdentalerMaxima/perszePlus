9338a005213218cfd5845b2bdf7d0b0c1030ba67
IdentalerMaxima <koppany.naray@gmail.com>
2024-08-09 13:29:10 +0200

    refactor: Update Settings form layout and styling

--- vite-project/src/components/forms/Settings.jsx
+++ vite-project/src/components/forms/Settings.jsx
@@ -7,7 +7,7 @@ export default function Settings() {
             <Box className="">
                 <h1 className="font-semibold">Notifications</h1>
 
-                <div className="mt-10 flex flex-col p-4  space-y-4">
+                <div className="mt-6 flex flex-col p-3 space-y-4">
 
                     <div className="flex flex-row items-center">
                         <input
@@ -37,6 +37,26 @@ export default function Settings() {
                         />
                         <p className="text-gray-500 ml-2"> Receive emails of notifications</p>
                     </div>
+
+                </div>
+
+                <div className="mt-40 flex items-center justify-end gap-x-6 border-t border-gray-90 pt-4">
+
+                    <button
+                        type="button"
+                        className="text-sm font-semibold leading-6 text-gray-900"
+                    //onClick={handleCancel}
+                    >
+                        Cancel
+                    </button>
+
+                    <button
+                        type="submit"
+                        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    //onClick={handleSubmit}
+                    >
+                        Save
+                    </button>
 
                 </div>
             </Box>
