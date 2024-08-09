e9e994e67dd16664c1ac6e8643867d117e3780bc
IdentalerMaxima <koppany.naray@gmail.com>
2024-08-09 13:08:27 +0200

    feat: Add settings form for notifications

---
+++ vite-project/src/components/forms/Settings.jsx
@@ -0,0 +1,45 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function Settings() {
+    return (
+        <div>
+            <Box className="">
+                <h1 className="font-semibold">Notifications</h1>
+
+                <div className="mt-10 flex flex-col p-4  space-y-4">
+
+                    <div className="flex flex-row items-center">
+                        <input
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                        />
+                        <p className="text-gray-500 ml-2"> Receive notifications of new events</p>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <input
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                        />
+                        <p className="text-gray-500 ml-2"> Receive email notifications of new posts</p>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <input
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                        />
+                        <p className="text-gray-500 ml-2"> Receive email notifications of new courses</p>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <input
+                            type="checkbox"
+                            className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                        />
+                        <p className="text-gray-500 ml-2"> Receive emails of notifications</p>
+                    </div>
+
+                </div>
+            </Box>
+        </div>
+    );
+}
