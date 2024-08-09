12cb7b29912d2482c99996ff397d57ce6c90a265
IdentalerMaxima <koppany.naray@gmail.com>
2024-08-09 18:59:40 +0200

    refactor: Update foreign key constraints in create_messages_table migration

--- perszePlus/database/migrations/2024_08_07_091947_create_messages_table.php
+++ perszePlus/database/migrations/2024_08_07_091947_create_messages_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('sender_id')->constrained('users');
-            $table->foreignId('receiver_id')->constrained('users');
+            $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('receiver_id')->constrained('users')->onDelete('cascade');
             $table->text('message');
             $table->timestamps();
         });
