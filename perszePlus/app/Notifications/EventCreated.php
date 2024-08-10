d24b6a5ae3178c2a90ebf40162183f80f8b63640
IdentalerMaxima <koppany.naray@gmail.com>
2024-08-10 17:11:20 +0200

    feat: Add EventCreated notification for sending email notifications

---
+++ perszePlus/app/Notifications/EventCreated.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EventCreated extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    protected $event;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct($event)
+    {
+        $this->event = $event;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        // Use environment URL if needed
+        $appUrl = config('app.url');
+        $eventUrl = $appUrl . '/calendar';
+
+        return (new MailMessage)
+                    ->subject('New Event Created: ' . $this->event->title)
+                    ->greeting('Hello ' . $notifiable->first_name . ' ' . $notifiable->last_name . ',')
+                    ->line('A new event has been created: ' . $this->event->title)
+                    ->line('Details: ' . $this->event->description)
+                    ->action('View Event', $eventUrl)
+                    ->line('Thank you for using our application!');
+    }
+}
