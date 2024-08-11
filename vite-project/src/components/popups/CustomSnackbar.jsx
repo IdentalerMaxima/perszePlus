e02ca166318fa57948a62fa0542f936f845a20ab
IdentalerMaxima <koppany.naray@gmail.com>
2024-08-11 18:36:17 +0200

    refactor: Rename SuccessSnackbar to CustomSnackbar and add severity prop

--- vite-project/src/components/popups/CustomSnackbar.jsx
+++ vite-project/src/components/popups/CustomSnackbar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Snackbar, Alert } from "@mui/material";
 
-export default function SuccessSnackbar({ open, message, onClose }) {
+export default function CustomSnackbar({ open, message, severity = "success", onClose }) {
     return (
         <Snackbar
             open={open}
             autoHideDuration={6000}
             onClose={onClose}
+            message={message} // Optional: Displays the message in case of any issue with Alert
         >
-            <Alert onClose={onClose} severity="success">
+            <Alert onClose={onClose} severity={severity}>
                 {message}
             </Alert>
         </Snackbar>
