70e70ec5a945c3991ff2e793b2886df2e187d3fa
IdentalerMaxima <koppany.naray@gmail.com>
2024-08-11 12:25:42 +0200

    feat: Add support for sending email notifications for new courses, posts, and events

--- perszePlus/app/Jobs/SendEmailJob.php
+++ perszePlus/app/Jobs/SendEmailJob.php
@@ -8,25 +8,28 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Notifications\EventCreated;
+use App\Notifications\NewPostNotification;
+use App\Notifications\NewCourseNotification;  // Add this line
 use App\Models\User;
-use Illuminate\Support\Facades\Notification;
 
 class SendEmailJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $user;
-    protected $event;
+    protected $item;
+    protected $type;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct(User $user, $event)
+    public function __construct(User $user, $item, $type)
     {
         $this->user = $user;
-        $this->event = $event;
+        $this->item = $item;
+        $this->type = $type;
     }
 
     /**
@@ -36,6 +39,12 @@ class SendEmailJob implements ShouldQueue
      */
     public function handle()
     {
-        $this->user->notify(new EventCreated($this->event));
+        if ($this->type === 'event') {
+            $this->user->notify(new EventCreated($this->item));
+        } elseif ($this->type === 'post') {
+            $this->user->notify(new NewPostNotification($this->item));
+        } elseif ($this->type === 'course') {
+            $this->user->notify(new NewCourseNotification($this->item)); // Add this line
+        }
     }
 }
