feb2e1c89f420f08425bf128e49736161bdf982d
Náray Koppány Titusz <koppany.naray@gmail.com>
2024-10-23 16:50:28 +0200

    chore: delete comment

--- vite-project/src/axios.js
+++ vite-project/src/axios.js
@@ -3,7 +3,6 @@ import router from './router';
 
 const axiosClient = axios.create({
     baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
-    //baseURL: 'http://localhost:8000/api',
 });
 
 axiosClient.interceptors.request.use((config) => {
